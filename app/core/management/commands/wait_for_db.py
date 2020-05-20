71e0390c829166778102607f8b246f749c8ec445
karimLa <kira00luffy@gmail.com>
2020-05-20 21:52:05 +0000

    fixed a mistake in github action

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,16 +5,17 @@ from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-  # Django command to pause execution until database is available
-
-  def handle(self, *args, **options):
-    self.stdout.write('Waiting for database...')
-    db_con = None
-    while not db_con:
-      try:
-        db_con = connections['default']
-      except OperationalError:
-        self.stdout.write('Database unavailable, waiting 1 second')
-        time.sleep(1)
-    
-    self.stdout.write(self.style.SUCCESS('Database is available'))
+    # Django command to pause execution until database is available
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for database...')
+        db_con = None
+        tried = 1
+        while not db_con and tried <= 20:
+            try:
+                db_con = connections['default']
+            except OperationalError:
+                self.stdout.write(f'Database unavailable, waiting {tried} second(s)')
+                time.sleep(1 * tried)
+                tried += 1
+        
+        self.stdout.write(self.style.SUCCESS('Database is available'))
