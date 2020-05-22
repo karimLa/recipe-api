d30ee6c00d35c29a68b9bf67a989ae5b9c6cffb0
karimLa <kira00luffy@gmail.com>
2020-05-22 21:13:11 +0000

    added working list tags endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -3,45 +3,47 @@ from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
   # Serializer for the users object
-  class Meta:
-    model = get_user_model()
-    fields = ('email', 'password', 'name')
-    extra_kwargs = {'password': { 'write_only': True, 'min_length': 5 }}
-  
-  def create(self, validated_data):
-    # Create a new user withe encrypted password and return it
-    return get_user_model().objects.create_user(**validated_data)
-  
-  def update(self, instance, validated_data):
-    # Update a user, setting the password correctly and return it
-    password = validated_data.pop('password', None)
-    user = super().update(instance, validated_data)
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'name')
+        extra_kwargs = {'password': { 'write_only': True, 'min_length': 5 }}
+    
+    def create(self, validated_data):
+        # Create a new user withe encrypted password and return it
+        return get_user_model().objects.create_user(**validated_data)
+    
+    def update(self, instance, validated_data):
+        # Update a user, setting the password correctly and return it
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
 
-    if password:
-      user.set_password(password)
-      user.save()
+        if password:
+            user.set_password(password)
+            user.save()
 
-    return user
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
-  # Serialize for the user authentication object
-  email = serializers.CharField()
-  password = serializers.CharField(
-    style={'input_style': 'password'},
-    trim_whitespace=False
-  )
+    # Serialize for the user authentication object
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_style': 'password'},
+        trim_whitespace=False
+    )
 
-  def validate(self, attrs):
-    # Validate and authenticate the user
-    email = attrs.get('email')
-    password = attrs.get('password')
+    def validate(self, attrs):
+        # Validate and authenticate the user
+        email = attrs.get('email')
+        password = attrs.get('password')
 
-    user = authenticate(request=self.context.get('request'), username=email, password=password)
-    if not user:
-      msg = _('Unable to authenticate with provided credentials')
-      raise serializers.ValidationError(msg, code='authentication')
-  
-    attrs['user'] = user
-    return attrs
+        user = authenticate(request=self.context.get('request'), username=email, password=password)
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+    
+        attrs['user'] = user
+        return attrs
